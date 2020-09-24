5f2959a90cc0104fea04bb04d6fe3514f431ec87
Jeremy Odell <jeremyodell@gmail.com>
2020-09-24 05:42:08 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            plugins: [
+              'postcss-import',
+              'tailwindcss',
+              'autoprefixer',
+            ],
+
+          }
+        }
+
+      }
+    ]
+  }
+};
